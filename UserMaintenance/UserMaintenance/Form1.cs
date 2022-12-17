29de778f4cbf8a51679f2eec2b01d9311c45b0a9
TUP <turcsanyipatrik@gmail.com>
2022-12-17 13:41:49 +0100

    4b

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -12,9 +12,19 @@ namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        List<Tick> ticks;
+        PortfolioEntities context = new PortfolioEntities();
         public Form1()
         {
             InitializeComponent();
+
+            ticks = context.Tick.ToList();
+            dataGridView1.DataSource = ticks;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
