aa735fac3815e4c035791c789cb828ee96844d78
TUP <turcsanyipatrik@gmail.com>
2022-12-17 13:54:50 +0100

    5

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.entities;
 
 namespace UserMaintenance
 {
@@ -14,12 +15,26 @@ namespace UserMaintenance
     {
         List<Tick> ticks;
         PortfolioEntities context = new PortfolioEntities();
+        List<PortfolioItem> portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
 
             ticks = context.Tick.ToList();
             dataGridView1.DataSource = ticks;
+
+
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = portfolio;
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
